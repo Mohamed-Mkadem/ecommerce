f589b88159dc9c6e19984eeaf89816a7c0091a5c
Mohamad Mkadem <mkademhamma19@gmail.com>
2026-06-18 02:54:01 +0100

    feat: implement Footer component and dynamic AppLogo component with multi-color support

--- resources/js/Components/AppLogo.vue
+++ resources/js/Components/AppLogo.vue
@@ -3,30 +3,16 @@ import { usePage } from "@inertiajs/vue3";
 
 const props = defineProps(["color"]);
 import whiteLogo from "@/assets/images/logo-white.webp";
-import goldLogo from "@/assets/images/logo-gold.webp";
 import blueLogo from "@/assets/images/logo-blue.webp";
-import blackLogo from "@/assets/images/logo-black.webp";
 </script>
 
 <template>
     <img
         class=""
-        :src="blackLogo"
-        alt="Logo patisserie boughanmi"
-        v-if="props.color == 'black'"
-    />
-    <img
-        class=""
         :src="whiteLogo"
         alt="Logo patisserie boughanmi"
-        v-else-if="props.color == 'white'"
-    />
-    <img
-        class=""
-        :src="blueLogo"
-        alt="Logo patisserie boughanmi"
-        v-else-if="props.color == 'blue'"
+        v-if="props.color == 'white'"
     />
 
-    <img class="" :src="goldLogo" alt="Logo patisserie boughanmi" v-else />
+    <img class="" :src="blueLogo" alt="Logo patisserie boughanmi" v-else />
 </template>
